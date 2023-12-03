7f41d55c22c1072f561d9cb9efdc2a564533ed90
David Nemeth <david.nemeth@healthcareinternational.com>
2023-12-03 18:34:06 +0000

    Create Insurance Model

---
+++ database/migrations/2023_12_03_183236_create_insurances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('insurances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('insurer_id');
+            $table->foreign('insurer_id')->references('id')->on('insurers')->onDelete('cascade');
+            $table->string('insurance_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('insurances');
+    }
+};
