dfa1aec8d642f35dc8586549ec966a9f8af1e8de
David Nemeth <david.nemeth@healthcareinternational.com>
2023-12-03 18:30:02 +0000

    Create ClientType Model

---
+++ database/migrations/2023_12_03_182822_create_client_types_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_type_name');
+        });
+
+        DB::table('client_types')->insert(
+            array(
+                [
+                    'client_type_name' => 'Corporate',
+                ],
+                [
+                    'client_type_name' => 'Individual',
+                ],
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_types');
+    }
+};
